4876eb252b46b2b2ab0e0951e5cd58744166fba6
MichalBogd <94784210+MichalBogd@users.noreply.github.com>
2022-03-05 18:09:55 +0100

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -13,18 +13,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-   @Override
+ @Override
     public void start(Stage stage) {
-        Label l = new Label("nic");
-        Label l2 = new Label("nic");
-        Label l3 = new Label("nic");
-        Label l4 = new Label("nic");
-        Label l5 = new Label("nic");
-        Label l6 = new Label("nic");
-        Label l7 = new Label("nic");
-        Label l8 = new Label("nic");
-        Label l9 = new Label("nic");
-        Label l10 = new Label("nic");
+        Label l = new Label("NESPLNĚNO");
+        Label l2 = new Label("NESPLNĚNO");
+        Label l3 = new Label("NESPLNĚNO");
+        Label l4 = new Label("NESPLNĚNO");
+        Label l5 = new Label("NESPLNĚNO");
+        Label l6 = new Label("NESPLNĚNO");
+        Label l7 = new Label("NESPLNĚNO");
+        Label l8 = new Label("NESPLNĚNO");
+        Label l9 = new Label("NESPLNĚNO");
+        Label l10 = new Label("NESPLNĚNO");
 
         TextField text = new TextField();
         TextField text2 = new TextField();
@@ -47,7 +47,6 @@ public class HelloApplication extends Application {
         CheckBox check8 = new CheckBox();
         CheckBox check9 = new CheckBox();
         CheckBox check10 = new CheckBox();
-
 
         Button button = new Button(" + ");
         button.relocate(190, 300);
@@ -86,37 +85,95 @@ public class HelloApplication extends Application {
         //https://stackoverflow.com/questions/20273820/how-to-copy-content-from-one-tab-to-another-in-javafx
         //https://www.educba.com/javafx-textfield/
         check.setOnAction((e) -> {
-            l.setText(text.getText());
+            l.setText(text.getText());check7.setOnAction((e2) -> {
+                l.setText("NESPLNĚNO");
+                check.setOnAction((e3) -> {
+                    l.setText(text.getText());
+                });
+            });
         });
         check2.setOnAction((e) -> {
             l2.setText(text2.getText());
+            check2.setOnAction((e2) -> {
+                l2.setText("NESPLNĚNO");
+                check2.setOnAction((e3) -> {
+                    l2.setText(text2.getText());
+                });
+            });
         });
         check3.setOnAction((e) -> {
             l3.setText(text3.getText());
+            check3.setOnAction((e2) -> {
+                l3.setText("NESPLNĚNO");
+                check3.setOnAction((e3) -> {
+                    l3.setText(text3.getText());
+                });
+            });
 
         });
         check4.setOnAction((e) -> {
             l4.setText(text4.getText());
+            check4.setOnAction((e2) -> {
+                l4.setText("NESPLNĚNO");
+                check4.setOnAction((e3) -> {
+                    l4.setText(text4.getText());
+                });
+            });
         });
         check5.setOnAction((e) -> {
             l5.setText(text5.getText());
+            check5.setOnAction((e2) -> {
+                l5.setText("NESPLNĚNO");
+                check5.setOnAction((e3) -> {
+                    l5.setText(text5.getText());
+                });
+            });
         });
         check6.setOnAction((e) -> {
             l6.setText(text6.getText());
+            check6.setOnAction((e2) -> {
+                l6.setText("NESPLNĚNO");
+                check6.setOnAction((e3) -> {
+                    l6.setText(text6.getText());
+                });
+            });
         });
         check7.setOnAction((e) -> {
             l7.setText(text7.getText());
+            check7.setOnAction((e2) -> {
+                l7.setText("NESPLNĚNO");
+                check7.setOnAction((e3) -> {
+                    l7.setText(text7.getText());
+                });
+            });
         });
         check8.setOnAction((e) -> {
             l8.setText(text8.getText());
+            check8.setOnAction((e2) -> {
+                l8.setText("NESPLNĚNO");
+                check8.setOnAction((e3) -> {
+                    l8.setText(text8.getText());
+                });
+            });
         });
         check9.setOnAction((e) -> {
             l9.setText(text9.getText());
+            check9.setOnAction((e2) -> {
+                l9.setText("NESPLNĚNO");
+                check9.setOnAction((e3) -> {
+                    l9.setText(text9.getText());
+                });
+            });
         });
         check10.setOnAction((e) -> {
             l10.setText(text10.getText());
+            check10.setOnAction((e2) -> {
+                l10.setText("NESPLNĚNO");
+                check7.setOnAction((e3) -> {
+                    l10.setText(text10.getText());
+                });
+            });
         });
-
 
         button.setOnAction((e) -> {
           //  TextField text11 = new TextField();
