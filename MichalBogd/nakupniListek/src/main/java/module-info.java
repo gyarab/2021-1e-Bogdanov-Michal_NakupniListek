f9c631809333e25c64286ca99a84f24c548830cd
MichalBogd <94784210+MichalBogd@users.noreply.github.com>
2022-03-18 11:02:35 +0100

    Delete MichalBogd directory

--- MichalBogd/nakupniListek/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.nakupnilistek {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.nakupnilistek to javafx.fxml;
-    exports com.example.nakupnilistek;
-}
